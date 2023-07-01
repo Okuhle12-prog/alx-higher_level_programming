3e5b95168672b98d0f568ff86157f0eeb7a16ca4
Okuhle12-prog <thandokuhledupont@gmail.com>
2023-07-01 07:18:53 -0700

    Python conditional statement

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # 6-print_comb3.py
 
 """Print all possible different combinations of two digits in ascending order.
@@ -10,4 +11,3 @@ for digit1 in range(0, 10):
             print("{}{}".format(digit1, digit2))
         else:
             print("{}{}".format(digit1, digit2), end=", ")
-
