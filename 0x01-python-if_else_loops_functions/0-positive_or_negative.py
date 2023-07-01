9b39e9e56c3f28963e485caf21bbeb4709484cec
Okuhle12-prog <thandokuhledupont@gmail.com>
2023-07-01 06:28:44 -0700

    Python conditional statement

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,13 +2,10 @@
 import random
 
 number = random.randint(-10, 10)
-# YOUR CODE HERE
-number = int(input(""))
+number = random.randint(-10, 10)
 if number > 0:
-    print(number, " is positive")
-elif number < 0:
-    print(number, " is negative")
+    print("{} is positive".format(number))
+elif number == 0:
+    print("{} is zero".format(number))
 else:
-    print(number, " is zero")
-
-
+    print("{} is negative".format(number))
